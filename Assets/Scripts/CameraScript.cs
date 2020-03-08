aadaa7bd4e5a96ad4680a3723dc8dd0fe443cbe1
KRB3AST <59933252+KRB3AST@users.noreply.github.com>
2020-03-07 23:19:39 -0500

    lol

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript: MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject player;
 
+
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
+        offset = transform.position - player.transform.position;
+    }
 
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,15 +8,45 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
 
     public float speed;
-    public Text score;
-    private int scoreValue = 0;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public AudioClip musicClipOne;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    private int count;
+    private int lives;
+    Animator anim; 
+    private bool facingRight = true;
+
+    private int level = 1;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        winText.text = "";
+        count = 0;
+        lives = 3;
+        SetLivesText();
+        SetCountText();
+        anim = GetComponent<Animator>();
+        musicSource.Play();
+        musicSource.clip = musicClipOne;
+        musicSource.loop = true;
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,31 +54,115 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-        if (Input.GetKey("escape"))
+        if (facingRight == false && hozMovement > 0)
         {
-            Application.Quit();
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
         }
 
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.collider.tag == "Coin")
+        if (other.gameObject.CompareTag("Coin"))
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+
+            SetLivesText();
+
         }
     }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+            winText.text = "You lose! Game created by Kyle Remy!";
+        }
+    }
+
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground")
         {
-            if(Input.GetKey(KeyCode.W))
+            anim.SetBool("Jump", false);
+
+            if (Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                anim.SetBool("Jump", true);
+
             }
+
         }
+        
+        
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 4 && level == 1)
+        {
+            level = 2;
+            transform.position = new Vector2(100.0f, 100.0f);
+            lives = 3;
+            SetLivesText();
+
+        }
+
+        if (count == 8)
+        {
+            winText.text = "You Win! Game created by Kyle Remy!";
+            musicSource.loop = false;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
 }
